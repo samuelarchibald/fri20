4bb76f1bc3996199d8ba986e917e1d0921c236c6
Samuel Clark <sam.clark@me.com>
2015-02-21 15:05:40 -0800

    Added names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,7 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
+	validates :desciption, presence: true
+	validates :image, presence: true
 end
