6f28da5cbe22185c171f01e633ae1d3e019516bb
Samuel Clark <sam.clark@me.com>
2015-02-20 18:10:45 -0800

    Added amazon s3 for paperclip....

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ Rails.application.configure do
 
   #set this to actuall hostname
   config.action_mailer.default_url_options = { host: 'localhost' }
+
+  #sets paperclip to upload images to amazon s3
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
